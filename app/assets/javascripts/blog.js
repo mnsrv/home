4e3bd55b6723ca2cd41aaf0a9f766e2fc5f15548
Aleksandr <am@idaproject.com>
2017-05-06 14:03:04 +0300

    Remove blog text

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.get(ghost.url.api('posts', {limit: 3})).done((data) => {
         var posts = []
         data.posts.forEach((post) => {
-            posts.push('<h2>' + post.title + '</h2><div>' + post.html + '</div>')
+            posts.push('<h2>' + post.title + '</h2>')
         })
         $('#blog').append(posts)
     });
