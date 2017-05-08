1ca776f9c4964fb5fc5bcd15c37830da2d3aa0db
Aleksandr <am@idaproject.com>
2017-05-08 20:08:05 +0300

    Update rails version

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Home
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.javascript_engine :js
+    end
   end
 end
