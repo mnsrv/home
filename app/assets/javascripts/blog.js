b702fe5b3d3af572459892af6932b85117fabcdb
Aleksandr <am@idaproject.com>
2017-05-08 22:40:24 +0300

    remove es6 feature

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.get(ghost.url.api('posts', {limit: 3})).done((data) => {
+    $.get(ghost.url.api('posts', {limit: 3})).done(function(data) {
         var posts = []
         data.posts.forEach(function(post) {
             posts.push('<h2>' + post.title + '</h2>')
