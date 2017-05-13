40f2af137c8c43dfb17f51df23b72ac033b02bd4
Aleksandr <am@idaproject.com>
2017-05-13 14:35:34 +0300

    Check if ghost is defined

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    $.get(ghost.url.api('posts', {limit: 3})).done(function(data) {
-        var posts = []
-        data.posts.forEach(function(post) {
-            posts.push('<h2>' + post.title + '</h2>')
-        })
-        $('#blog').append(posts)
-    });
+    if (typeof ghost !== 'undefined') {
+        $.get(ghost.url.api('posts', {limit: 3})).done(function(data) {
+            var posts = []
+            data.posts.forEach(function(post) {
+                posts.push('<h2>' + post.title + '</h2>')
+            })
+            $('#blog').append(posts)
+        });
+    }
 });
